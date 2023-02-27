e54fff6e8940681e8abe63eb7136f1125995b22d
heren18 <126468790+heren18@users.noreply.github.com>
2023-02-27 14:35:41 +0300

    Update patika.sql

--- patika.sql
+++ patika.sql
@@ -1,1 +1,7 @@
+ÖDEV-1
 
+Select title, description From film
+Select * from film Where lenght>60 And lenght<75; 
+Select * from film where (rental_rate = 0.99) And (replacement_cost = 12.99 OR replacement_cost = 28.99);
+Select last_name from customer where first_name = 'Mary';
+Select * from film where not (length > 50) and not (rental_rate = '2.99' or rental_rate = '4.99');
